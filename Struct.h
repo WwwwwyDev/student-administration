9fdb847de3674be57d0cce2b8660649bf05c3ddf
wuwenyi <773323518@qq.com>
2020-03-02 11:47:10 +0800

    set struct

---
+++ Struct.h
@@ -0,0 +1,11 @@
+typedef struct Student
+{   int  m_nsign;
+	char m_strName[20]; //学生基本信息 
+	char m_strGrade[20];
+	char m_strClass[20];
+	int  m_nMath; //各科成绩 
+	int  m_nChinese;
+	int  m_nEnglish;
+	int  m_nComputer;
+	link *m_pnext;
+}Stu;
